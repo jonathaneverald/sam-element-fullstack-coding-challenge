c0d5e0ac958ea1e683339233307ea7c2d2b862ef
jonathaneverald <jonathaneverald@gmail.com>
2024-10-27 04:21:16 +0700

    feat: setup validations, middlewares, response error, type for user

---
+++ back-end/src/validation/note-validation.ts
@@ -0,0 +1,21 @@
+import { z, ZodType } from "zod";
+
+export class NoteValidation {
+	static readonly CREATE: ZodType = z.object({
+		title: z.string().min(1).max(100),
+		content: z.string().min(1).max(300),
+	});
+
+	static readonly UPDATE: ZodType = z.object({
+		id: z.number().positive(),
+		title: z.string().min(1).max(100).optional(),
+		content: z.string().min(1).max(100).optional(),
+	});
+
+	static readonly SEARCH: ZodType = z.object({
+		title: z.string().min(1).max(100).optional(),
+		content: z.string().min(1).max(100).optional(),
+		page: z.number().min(1).positive(),
+		size: z.number().min(1).max(100).positive(),
+	});
+}

7f9b3df8e775dfec896c414168378e5a8cf82435
jonathaneverald <jonathaneverald@gmail.com>
2024-10-28 02:19:18 +0700

    feat: add cors

--- back-end/src/application/web.ts
+++ back-end/src/application/web.ts
@@ -2,8 +2,12 @@ import express from "express";
 import { publicRouter } from "../route/public-api";
 import { errorMiddleware } from "../middleware/error-middleware";
 import { apiRouter } from "../route/api";
+import corsMiddleware from "../middleware/cors";
 
 export const web = express();
+
+web.use(corsMiddleware);
+web.options("/api/*", corsMiddleware);
 web.use(express.json());
 web.use(publicRouter);
 web.use(apiRouter);
---
+++ back-end/src/middleware/cors.ts
@@ -0,0 +1,25 @@
+// import cors from "cors";
+
+const cors = require("cors");
+
+const allowedOrigins = ["https://sam-element-fullstack-coding-challenge-frontend.vercel.app", "http://localhost:3000"];
+
+const corsMiddleware = cors({
+	origin: (origin: string | undefined, callback: (error: Error | null, allow?: boolean) => void) => {
+		// Allow requests with no origin (like mobile apps, curl, etc.)
+		if (!origin) return callback(null, true);
+		if (allowedOrigins.includes(origin)) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+	//   credentials: true,
+	methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+	allowedHeaders: ["Content-Type", "X-API-TOKEN", "Authorization", "Accept"],
+	exposedHeaders: ["X-API-TOKEN"],
+});
+
+console.log("CORS Middleware Applied"); // Add this for debugging
+
+export default corsMiddleware;
